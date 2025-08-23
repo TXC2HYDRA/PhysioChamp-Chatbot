81eb65ced863000615f1694cfd9c663aa020c845
Nachiket Challawar <145981533+TXC2HYDRA@users.noreply.github.com>
2025-08-24 03:08:26 +0530

    Update connection.py

--- champ/db/connection.py
+++ champ/db/connection.py
@@ -3,10 +3,10 @@ import mysql.connector
 
 def get_connection():
     return mysql.connector.connect(
-        host=os.getenv("MYSQL_HOST", "10.171.28.60"),
-        port=int(os.getenv("MYSQL_PORT", "3306")),
-        database=os.getenv("MYSQL_DB", "Physiochamp"),
-        user=os.getenv("MYSQL_USER", "rag_user"),
-        password=os.getenv("MYSQL_PASSWORD", "root"),
+        host=os.getenv("MYSQL_HOST", "physiochamp-physiochamp.b.aivencloud.com"),
+        port=int(os.getenv("MYSQL_PORT", "27951")),
+        database=os.getenv("MYSQL_DB", "physiochamp"),
+        user=os.getenv("MYSQL_USER", "avnadmin"),
+        password=os.getenv("MYSQL_PASSWORD", "AVNS_0LnHsd0Wk3utZWoZix1"),
         autocommit=True,
     )
